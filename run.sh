0ee7a2d2ba6aa1d57b10f308c2db2b30deae767f
Leonard <Leonhard.Blazevic@hensoldt-cyber.com>
2019-09-20 10:42:31 +0200

    [FIX] SEOS-483: Updating xml path for out of source build.

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ OUTPUT_PATH=$3
 
 # calling the python script which parses the xml with key info and 
 # calls the provisioning tool with proper arguments
-python xmlParser.py $KEYS_XML_INPUT $PROVISIONING_TOOL
+python ./src/xmlParser.py $KEYS_XML_INPUT $PROVISIONING_TOOL
 
 # moves the created binary to the desired path passed as an argument
 mv ./nvm_06 $OUTPUT_PATH
