d8d53cf92e511f327aa28a6b52f6061919de475d
Axel Heider <axel.heider@hensoldt-cyber.com>
2019-11-17 18:04:36 +0100

    [DEV] improve subprocess usage, fix output newline handling

--- xmlParser.py
+++ xmlParser.py
@@ -1,6 +1,6 @@
 import sys
 import xml.etree.ElementTree as ET
-from subprocess import Popen, PIPE
+import subprocess
 
 keysXmlPath = sys.argv[1]           # path to the xml file that contains the key information
 toolExecutablePath = sys.argv[2]    # path to the keystore provisioning tool executable
@@ -12,7 +12,12 @@ for elem in root:
     params = []
     for node in elem:
         params.append(node.text)
-    
-    process = Popen([toolExecutablePath] + params, stdout=PIPE)
+
+    process = subprocess.Popen(
+                [toolExecutablePath] + params,
+                stdout=subprocess.PIPE,
+                shell=False,
+                universal_newlines=True)
+
     (output, err) = process.communicate()
     print(output)
